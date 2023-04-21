cc9871177009537cd502dfd7a26540cdd4a6b6d4
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2023-04-21 14:43:45 +0530

    First program for class

--- 1.py
+++ 1.py
@@ -1,10 +1,9 @@
-#Q1. Find the largest among two numbers
+num1 = int(input("1st number: "))
+num2 = int(input("2nd number: "))
 
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
-if a>b:
-    print("The largest number is: ",a)
-if a == b:
-    print("Both numbers are equal")
-else:
-    print("The largest number is: ",b)
+if num1 > num2:
+    print(num1)
+if num2 > num1:
+    print(num2)
+if num1 == num2:
+    print("equal")
