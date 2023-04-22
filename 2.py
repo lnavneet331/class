fbb2e4117727cbede86bad836694d897745df682
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2023-04-22 11:45:45 +0530

    2nd program for class

--- 2.py
+++ 2.py
@@ -1,6 +1,14 @@
-# Q2. Check whether a number is divisible by 5 and 11
-a = int(input("Enter the number: "))
-if a%5 == 0 and a%11 == 0:
-    print("The number is divisible by 5 and 11")
+num = int(input("2nd number: "))
+
+if num%5==0 and num%11==0:
+    print("Yes")
 else:
-    print("The number is not divisible by 5 and 11")
+    print("No")
+
+# if num%5==0:
+#     if num%11== 0:
+#         print("Yes")
+#     else:
+#         print("No")
+# else:
+#     print("No")
