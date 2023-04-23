c02bc76b7daff137ae3ca75bc80bb30a0f3417ca
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2023-04-23 10:15:45 +0530

    3rd program for class

--- 3.py
+++ 3.py
@@ -1,9 +1,8 @@
-# Check whether a triangle is valid or not with given sides
-a = int(input("Enter the first side: "))
-b = int(input("Enter the second side: "))
-c = int(input("Enter the third side: "))
+a = int(input("Enter side 1: "))
+b = int(input("Enter side 2: "))
+c = int(input("Enter side 3: "))
+
 if a+b>c and b+c>a and c+a>b:
-    print("The triangle is valid")
+    print("Yes")
 else:
-    print("The triangle is not valid")
-    
+    print("No")
