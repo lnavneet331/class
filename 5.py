33ebb7728f742bf3059e1c2678d8ddb113dd05b0
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2023-04-25 00:30:15 +0530

    5th program for class

--- 5.py
+++ 5.py
@@ -1,7 +1,7 @@
-#check whether a number is even or odd
-a = int(input("Enter the number: "))
-if a%2 == 0:
-    print("The number is even")
+year=int(input("enter the year :"))
+if year%4==0 and year%400==0:
+    print("it is a leap year")
+elif year%100:
+    print("it is not a leap year")
 else:
-    print("The number is odd")
-    
+    print("it is not a leap year")
