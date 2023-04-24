5b402f022ac8935dbbd755a16d06ad965059b678
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2023-04-24 20:05:45 +0530

    4th program for class

--- 4.py
+++ 4.py
@@ -1,14 +1,5 @@
-#find the largest among 3 numbers
-a = int(input("Enter the first number: "))
-b = int(input("Enter the second number: "))
-c = int(input("Enter the third number: "))
-if a>b and a>c:
-    print("The largest number is: ",a)
-if b>a and b>c:
-    print("The largest number is: ",b)
-if c>a and c>b:
-    print("The largest number is: ",c)
-if a == b and b == c:
-    print("All the numbers are equal")
+year = int(input("Enter: "))
+if year%4==0 and year%100!=0 or year%400==0:
+    print("Yes")
 else:
-    print("The largest number is: ",c)
+    print("NO")
