3502f89732f20a376027a9424bee407aa2f7bb61
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2023-04-26 09:30:15 +0530

    6th program for class

--- 6.py
+++ 6.py
@@ -1,16 +1,8 @@
-#check whether a given year is leap year or not
-a = int(input("Enter the year: "))
-if a%4 == 0 and a%100 != 0 or a%400 == 0:
-    print("The year is leap year")
-else:
-    print("This is not a leap year")
+num = int(input("Enter year: "))
 
-if a % 4 == 0: 
-    if a % 400 == 0: 
-        print("This is a leap year")
-    elif a % 100 == 0:
-        print("This is not a leap year")
-    else:
-        print("This is a leap year")
+if num%4==0 and not num%100==0:
+    print("Yes")
+elif num%400==0:
+    print("Yes")
 else:
-    print("This is not a leap year")
+    print("No")
